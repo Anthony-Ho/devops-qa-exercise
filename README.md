3050f1b43151e681f1bed2e37eca662d01083139
Anthony Ho <anthony.ho.wc@gmail.com>
2023-06-21 17:02:48 +0100

    work during weekend.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Terraform Exercise for Udacity Devops
 
 **You need to create a `.tfvars` file for the Azure Service Principal information.**
+
+**- For new python virtual environment, need to create within this working directory if VSCode is used.**
+- http://automationpractice.com is invalid.
+- 
--- intro-to-selenium.py
+++ intro-to-selenium.py
@@ -1,7 +1,16 @@
 # #!/usr/bin/env python
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Start the browser and navigate to http://automationpractice.com/index.php.
-driver = webdriver.Chrome()
-driver.get('http://automationpractice.com/index.php')
-driver.find_element_by_css_selector("input[id='search_query_top']").send_keys("t shirt")
+driver = webdriver.Firefox()
+driver.get('https://www.uniqlo.com/uk/en/home')
+
+driver.implicitly_wait(10)
+reject_button = driver.find_element(By.CSS_SELECTOR, "#onetrust-reject-all-handler")
+reject_button.click()
+
+driver.find_element(By.CLASS_NAME,"algolia-search-input").send_keys("shirt")
+search_button = driver.find_element(By.CSS_SELECTOR, "button.algolia-search-button")
+search_button.click()
+
